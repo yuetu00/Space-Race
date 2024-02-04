f5b656c825fed85534417ef564ac8b5fc8bdae0e
yuetu00 <tiffany_gao@brown.edu>
2024-02-03 21:54:12 -0500

    added main screen

--- app/src/main/java/com/example/myapplication/LoadingPage.kt
+++ app/src/main/java/com/example/myapplication/LoadingPage.kt
@@ -1,19 +1,30 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.Image
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class LoadingPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +35,38 @@ class LoadingPage : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(Color.White)
-                        .padding(150.dp),
-
+                        .padding(150.dp, 250.dp),
+                ) {
+                    // Use Column to arrange Image and Text vertically
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                    // Your Compose UI components go here
-                    Image(
-                        painter = painterResource(id = R.drawable.loading_earth),
-                        contentDescription = null, // Provide a proper content description
-                        modifier = Modifier.size(100.dp)
-                    )
+                        Image(
+                            painter = painterResource(id = R.drawable.loading_earth),
+                            contentDescription = null,
+                            modifier = Modifier.size(100.dp),
+                        )
+                        Text(
+                            text = "loading...",
+                            modifier = Modifier.padding(top = 16.dp),
+                            style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    }
                 }
             }
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@LoadingPage, MainPage::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainPage.kt
+++ app/src/main/java/com/example/myapplication/MainPage.kt
@@ -3,16 +3,20 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,20 +42,14 @@ class MainPage : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(brush = gradientBrush)
-                        .padding(110.dp),
+                        .padding(150.dp),
 
                     ) {
                     // Text composable in the top-middle of the screen
-                    Text(
-                        text = "Space Race",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 200.dp),
-                        style = TextStyle(
-                            color = Color.Blue,
-                            fontSize = 30.sp, // Adjust the font size as needed
-                            fontWeight = FontWeight.Bold
-                        )
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null,
+                        modifier = Modifier.size(500.dp).scale(2.5f),
                     )
                 }
             }
